744d9bd1b80942557d262aa8b209b8e84ce82c68
joseluisbezerra <luisbezerra.work@gmail.com>
2021-07-13 15:18:42 -0300

    Ajustando as permissoes para apenas um usuario admin conseguir criar/deletar usuarios e editar a empresa

--- accounts/permitions.py
+++ accounts/permitions.py
@@ -1,16 +1,19 @@
 from rest_framework import permissions
 
 
-class IsUserOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow user of an object to edit it.
-    """
+class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.id == request.user.id
+        return obj.id == request.user.id and request.method != 'DELETE' or request.user.is_staff
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_staff
